fb5d1353f331de0803b2c7e1d89feebe3a28eec7
naveen6368 <badigernaveen660@gmail.com>
2026-04-20 17:39:48 +0530

    initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://zoo-management-system-1-0x98.onrender.com",
 });
 
 export default API;
