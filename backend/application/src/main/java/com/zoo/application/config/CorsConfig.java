f7b26739ade506c33b08d5295c2536f7ad367777
naveen6368 <badigernaveen660@gmail.com>
2026-04-20 16:47:47 +0530

    initial commit

--- backend/application/src/main/java/com/zoo/application/config/CorsConfig.java
+++ backend/application/src/main/java/com/zoo/application/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://zoo-management-project.netlify.app/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
